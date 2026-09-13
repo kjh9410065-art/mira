960ba7d0881d95ac93578400a0933522fb7aad76
kjh9410065-art <kjh9410065@gmail.com>
2026-09-13 17:44:12 +0900

    Mira 프로그램 삭제

--- image-bridge.js
+++
@@ -1,5 +0,0 @@
-// Cloudflare Worker에서 생성된 이미지 연결 스크립트를 HTML에 주입합니다.
-const IMAGE_SCRIPT = `<script src="/assets/site-images.js"></script>`;
-export function addImageBridge(html) {
-  return html.replace("</head>", `${IMAGE_SCRIPT}</head>`);
-}
